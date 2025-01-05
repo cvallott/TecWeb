9ea136e8ed7b1e37e5f91d2dcbf05d942bee3f19
Francesco Pozzobon <francesco.pozzobon.1@studenti.unipd.it>
2025-01-05 11:58:24 +0100

    gestione area riservata, inserimento carrello in menu

--- src/script/PHP/gestioneCarrello.php
+++ src/script/PHP/gestioneCarrello.php
@@ -18,6 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } elseif ($_POST['azione'] === 'rimuovi' && $id) {
         rimuoviDalCarrello($id);
     }
+
 }
 
 if (!isset($_SESSION['carrello'])) {
