ae61256e80ef59fd257a8d030fd8e018f8ff6126
Andrea Santi <andrea.santi.5@studenti.unipd.it>
2025-01-10 18:29:09 +0100

    add surname

--- src/conferma-ordine.php
+++ src/conferma-ordine.php
@@ -29,6 +29,6 @@ $template = str_replace('[header]', $header, $template);
 $template = str_replace('[oraridisp]', $selectOrari, $template);
 $template = str_replace('[footer]', $footer, $template);
 $template = str_replace('[nameClient]', $_SESSION['nome'], $template);
-//$template = str_replace('[surnameClient]', $_SESSION['cognome'], $template);
+$template = str_replace('[surnameClient]', $_SESSION['cognome'], $template);
 
 echo $template;
