b07ef946fe1a3a99e5eb633ff17617d3832e5582
Francesco Pozzobon <francesco.pozzobon.1@studenti.unipd.it>
2025-01-16 17:07:00 +0100

    errorPage

--- src/errore.php
+++ src/errore.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "ERROREEEEE";
+include_once 'template/components/loadComponents.php';
+$template = file_get_contents('template/pageTemplate/error/genericErrorTemplate.html');
+
+$header = printHeader();
+$footer = printFooter();
+
+$template = str_replace('[header]', $header, $template);
+$template = str_replace('[footer]', $footer, $template);
+
+echo $template;
